d6e6f16e50a63fcbd987f65bf2a48648cab1e9fa
fih205 <fih205@nyu.edu>
2020-08-27 21:43:25 -0700

    Modified guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
+
+echo "Guess how many files are in the current directory?"
+read guessnum
+
+if [[ $guessnum -eq 3]]
+then
+  echo "You are right!"
+elif [[ $guessnum -gt 3 ]]
+then
+  echo "The number is too big. Guess again."
+else
+  echo "The number is too big. Guess again."
+fi
